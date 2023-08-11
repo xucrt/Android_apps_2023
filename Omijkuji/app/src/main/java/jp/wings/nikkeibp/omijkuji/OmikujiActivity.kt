54bf9246bbdf839a4b54247e0c090253f589c911
xucrt <xuchuanru@gmail.com>
2023-08-11 23:11:37 +0900

    20230811_1st

--- Omijkuji/app/src/main/java/jp/wings/nikkeibp/omijkuji/OmikujiActivity.kt
+++ Omijkuji/app/src/main/java/jp/wings/nikkeibp/omijkuji/OmikujiActivity.kt
@@ -3,9 +3,17 @@ package jp.wings.nikkeibp.omijkuji
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import jp.wings.nikkeibp.omijkuji.databinding.FortuneBinding
 import jp.wings.nikkeibp.omijkuji.databinding.OmikujiBinding
 
 class OmikujiActivity : AppCompatActivity() {
+
+    //P169 おみくじ棚の配列
+    val omikujiShelf = Array<OmikujiParts>(20)
+    { OmikujiParts(R.drawable.result2, R.string.contents1)}
+
+    //P169おみくじ番号保管用
+    var omikujiNumber = -1
 
     val omikujiBox = OmikujiBox() //P152
 
@@ -17,6 +25,21 @@ class OmikujiActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         omikujiBox.omikujiView = binding.imageView //P152
+
+        //P170 おみくじ棚の準備
+        omikujiShelf[0].drawID = R.drawable.result1
+        omikujiShelf[0].fortuneID = R.string.contents2
+
+        omikujiShelf[1].drawID = R.drawable.result3
+        omikujiShelf[1].fortuneID = R.string.contents9
+
+        omikujiShelf[2].fortuneID = R.string.contents3
+        omikujiShelf[3].fortuneID = R.string.contents4
+        omikujiShelf[4].fortuneID = R.string.contents5
+        omikujiShelf[5].fortuneID = R.string.contents6
+        omikujiShelf[6].fortuneID = R.string.contents7
+        omikujiShelf[7].fortuneID = R.string.contents8
+        omikujiShelf[8].fortuneID = R.string.contents10
 
         /*
         //文字表示
@@ -76,5 +99,23 @@ class OmikujiActivity : AppCompatActivity() {
         binding.imageView.startAnimation(set)
         */
         //binding.imageView.setImageResource(R.drawable.result1)
+    }
+
+    //P170
+    fun drawResult(){
+
+        //おみくじ番号を取得
+        omikujiNumber = omikujiBox.number
+
+        //おみくじ棚の配列から、omikujiPartsを取得
+        val op = omikujiShelf[omikujiNumber]
+
+        //レイアオウトを運勢表示に変更する
+        val fortuneBinding = FortuneBinding.inflate(layoutInflater)
+        setContentView(fortuneBinding.root)
+
+        //画像とテキストを変更する。
+        fortuneBinding.imageView2.setImageResource(op.drawID)
+        fortuneBinding.textView.setText(op.fortuneID)
     }
 }
---
+++ Omijkuji/app/src/main/java/jp/wings/nikkeibp/omijkuji/OmikujiParts.kt
@@ -0,0 +1,3 @@
+package jp.wings.nikkeibp.omijkuji
+
+data class OmikujiParts(var drawID: Int, var fortuneID: Int)
